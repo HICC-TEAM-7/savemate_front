d936e85cb53f5a5972f590f502f510e68af28f62
범수 김 <gnh08033@naver.com>
2025-08-08 21:25:31 +0900

    feat: 사이드바 UI 수정

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,8 +1,18 @@
 const Sidebar = () => {
   return (
-    <div>
-      <h2>Sidebar</h2>
-    </div>
+    <aside className="w-48 fixed top-0 left-0 h-screen bg-gray-800 text-white flex flex-col justify-between p-4">
+      <div className="mt-8 mb-8 text-xl font-bold tracking-wide text-center">save mate</div>
+
+      <ul className="space-y-4">
+        <li className="hover:text-yellow-300 cursor-pointer flex justify-center">MAIN</li>
+        <li className="hover:text-yellow-300 cursor-pointer flex justify-center">MISSON</li>
+        <li className="hover:text-yellow-300 cursor-pointer flex justify-center">FRIEND</li>
+      </ul>
+
+      <ul className="pt-2 mb-8">
+        <li className="hover:text-yellow-300 cursor-pointer flex justify-center">LOGOUT</li>
+      </ul>
+    </aside>
   );
 };
 
