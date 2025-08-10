249fe98e529c667b019af042140c7d326cda618e
범수 김 <gnh08033@naver.com>
2025-08-10 17:17:03 +0900

    feat: 사이드바 UI 수정 및 page 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from "react-router-dom";
+import Sidebar from './components/layout/Sidebar';
 import Home from './pages/Home';
 import Mission from './pages/Mission';
 import Friend from './pages/Friend';
@@ -6,8 +7,12 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const location = useLocation();
+  const hideSidebar = ["/login", "/signup"].includes(location.pathname);
+
   return (
     <div className="min-h-screen app-background">
+      {!hideSidebar && <Sidebar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mission" element={<Mission />} />
